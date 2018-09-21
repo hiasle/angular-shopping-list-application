34fe4f75109656503b692e0a92b9aa0203fa8070
Fikret Ceylan <ofcyln@gmail.com>
2018-09-21 23:31:36 +0300

    royalty free images added to the recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,15 +8,12 @@ export class RecipeService {
         new Recipe(
             'Fooled Chicken Tears',
             'A poor chicken has been fooled correctly and gathered its tears as trophy.',
-            'http://s3.amazonaws.com/finecooking.s3.' +
-                'tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-' +
-                'mustard-rosemary-chicken-recipe-alt-main.jpg',
+            '../assets/image/chicken-close-up-992043.jpg',
         ),
         new Recipe(
             'VegiVigi',
             'The vegs as you know.',
-            'https://static1.squarespace.com/static/' +
-                '56d9cbd420c647c7373d4d82/t/58ed2e6386e6c02d633533ef/1495120065012/veggies.jpg',
+            '../assets/image/appetizer-background-cuisine-326281.jpg',
         ),
     ];
 
