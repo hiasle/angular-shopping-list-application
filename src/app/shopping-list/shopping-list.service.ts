3609a7d59bef947f754d10ef84ba2049b3fc74ef
Fikret Ceylan <ofcyln@gmail.com>
2018-10-02 22:12:58 +0300

    updating existing form items instead create copy of them

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -35,4 +35,10 @@ export class ShoppingListService {
 
         this.ingredientsChanged.next(this.ingredients.slice());
     }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
