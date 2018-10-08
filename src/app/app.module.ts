ce4b2bf27128500ed5501c3c3549d0632511f97d
Fikret Ceylan <ofcyln@gmail.com>
2018-10-08 22:34:06 +0300

    module optimizations and arrangement of shared module and seperation of modules via its usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 ///<reference path="../../node_modules/@angular/common/http/src/client.d.ts"/>
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/directives/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { HttpClientModule } from '@angular/common/http';
 import { RecipeService } from './recipes/recipe.service';
 import { HttpRequestService } from './shared/services/http-request.service';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -29,18 +24,18 @@ import { AuthGuardService } from './auth/auth-guard.service';
         AppComponent,
         HeaderComponent,
         ShoppingListComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
         ShoppingEditComponent,
-        DropdownDirective,
-        RecipeStartComponent,
-        RecipeEditComponent,
         SignupComponent,
         SigninComponent,
     ],
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RecipesModule,
+        AppRoutingModule,
+        SharedModule,
+    ],
     providers: [
         ShoppingListService,
         RecipeService,
