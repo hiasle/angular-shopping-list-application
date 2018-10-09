567fbc7dfe3fa9c1297f0c5f5618d03f705deb83
Fikret Ceylan <ofcyln@gmail.com>
2018-10-09 09:34:58 +0300

    created shoppingListModule to declare appModule concisely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,7 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { HttpRequestService } from './shared/services/http-request.service';
@@ -18,21 +16,16 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        ShoppingListComponent,
-        ShoppingEditComponent,
-        SignupComponent,
-        SigninComponent,
-    ],
+    declarations: [AppComponent, HeaderComponent, SignupComponent, SigninComponent],
     imports: [
         BrowserModule,
         FormsModule,
         HttpClientModule,
         RecipesModule,
+        ShoppingListModule,
         AppRoutingModule,
         SharedModule,
     ],
