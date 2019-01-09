027e4ce7c24db7b856c31e97103dad77f38e75c0
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-01-09 13:51:08 +0100

    auth store and app store implementations

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -5,7 +5,7 @@ import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from '../store/shopping-list.actions';
-import * as fromShoppingList from '../store/shopping-list.reducers';
+import * as fromApp from '../../store/app.reducers';
 
 @Component({
     selector: 'app-shopping-edit',
@@ -20,7 +20,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     editMode = false;
     editedItem: Ingredient;
 
-    constructor(private store: Store<fromShoppingList.AppState>) {}
+    constructor(private store: Store<fromApp.AppState>) {}
 
     ngOnInit() {
         this.subscription = this.store.select('shoppingList').subscribe((data) => {
